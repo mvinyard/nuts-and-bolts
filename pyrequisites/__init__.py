9e6ec2017a4578367a740b82f7ba1a8ba8732674
Michael Vinyard <47393421+mvinyard@users.noreply.github.com>
2021-12-13 19:52:30 -0500

    Update __init__.py

--- pyrequisites/__init__.py
+++ pyrequisites/__init__.py
@@ -2,3 +2,4 @@
 
 from ._create_empty_dict import _create_empty_dict as create_empty_dict
 from ._flexible_multilevel_mkdir import _flexible_multilevel_mkdir as mkdir_flex
+from ._get_parent_directory import _get_parent_directory as parent_dir
