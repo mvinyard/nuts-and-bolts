62a33243e13777b952263c4ed3a83bc5624d8703
mvinyard <mvinyard@broadinstitute.org>
2022-02-28 17:49:56 +0000

    adding google cloud command builder

--- setup.py
+++ setup.py
@@ -6,16 +6,16 @@ import sys
 
 setup(
     name="pyrequisites",
-    version="0.0.2",
+    version="0.0.1",
     python_requires=">3.7.0",
     author="Michael E. Vinyard - Harvard University - Massachussetts General Hospital - Broad Institute of MIT and Harvard",
     author_email="mvinyard@broadinstitute.org",
     url="",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    description="pyrequisites",
+    description="Python Developer Kit",
     packages=[
-        "pyrequisites",
+        "pydk",
     ],
     install_requires=[
         "licorice>=0.0.2",
