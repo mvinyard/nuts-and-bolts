3a182e46a93a44aac91f577e7f3879d1a12cb074
mvinyard <mvinyard@broadinstitute.org>
2022-03-01 04:22:42 +0000

    additional funcs

--- pydk/__init__.py
+++ pydk/__init__.py
@@ -3,3 +3,6 @@
 from ._create_empty_dict import _create_empty_dict as create_empty_dict
 from ._flexible_multilevel_mkdir import _flexible_multilevel_mkdir as mkdir_flex
 from ._get_parent_directory import _get_parent_directory as parent_dir
+from ._smooth_1d_array import _smooth_1d_array as smooth
+from ._mk_GCP_command import _mk_GCP_command as gcp
+from ._load_pickled import _load_pickled as load_pickled
