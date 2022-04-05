b6f26599f8e684ee3a15c7f849385266bce2be7a
mvinyard <mvinyard@broadinstitute.org>
2022-04-05 21:45:41 +0000

    remove scaler from min-max normalize

--- pydk/_min_max_normalize.py
+++ pydk/_min_max_normalize.py
@@ -1,9 +1,9 @@
 
-def _min_max_normalize(arr, scaler=1):
+def _min_max_normalize(arr):
     
-    """Min-max normalize and optionally scale entire result."""
+    """Min-max normalize array."""
     
     array_min =  arr.min()
     array_max = arr.max()
     
-    return ((arr - array_min) / (array_max - array_min)) * scaler
+    return ((arr - array_min) / (array_max - array_min))
