71b9d3364f780024a6079408101bbfe4bd4acb14
Michael Vinyard <47393421+mvinyard@users.noreply.github.com>
2022-09-02 20:16:21 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import sys
 
 setup(
     name="pydk",
-    version="0.0.52",
+    version="0.0.53",
     python_requires=">3.7.0",
     author="Michael E. Vinyard - Harvard University - Massachussetts General Hospital - Broad Institute of MIT and Harvard",
     author_email="mvinyard@broadinstitute.org",
@@ -16,6 +16,7 @@ setup(
     description="Python Developer Kit",
     packages=[
         "pydk",
+        "pydk._conda_package_installer",
     ],
     install_requires=[
         "licorice_font>=0.0.3",
