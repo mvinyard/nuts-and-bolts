2dccaca53be955df8b0189cd185427b44aca0a56
mvinyard <mvinyard@broadinstitute.org>
2022-09-05 20:58:33 -0600

    some fixes to CPI

--- pydk/_conda_package_installer/_conda_package_installer.py
+++ pydk/_conda_package_installer/_conda_package_installer.py
@@ -19,18 +19,28 @@ class conda_package_installer:
         self._conda_list_df = _conda_list_to_pandas_df(self._conda_list_tmp_path)
 
     def check_installation(self):
+        
+        self.conda_list()
         self.installed = _check_installation(
             self._conda_list_df, pkg_name=self._pkg_name
         )
-        if not self.installed:
-            self.do_installation()
-
-    def do_installation(self):
+        print("Installed: {}".format(self.installed))
+        
+    def _do_installation(self):
 
         self._download_log = _do_install_conda_pkg(self._channel, self._pkg_name)
         self.installed, self._trouble_shoot_msg = _check_download_log(
             self._download_log
+        ) 
+        
+    def install(self):
+        
+        self.conda_list()
+        self.installed = _check_installation(
+            self._conda_list_df, pkg_name=self._pkg_name
         )
+        if not self.installed:
+            self._do_installation()
 
     def remove_package(self):
         self._rm_log, self.installed = _remove_package(self._pkg_name)
--- pydk/_conda_package_installer/_supporting_functions.py
+++ pydk/_conda_package_installer/_supporting_functions.py
@@ -53,19 +53,17 @@ def _check_installation(conda_list_df, pkg_name):
 def _do_install_conda_pkg(channel, package_name):
 
     package_name_ = licorice_font.font_format(package_name, ["BOLD", "GREEN"])
-
     download_message = "Installing {} via conda... this may take a few seconds...".format(
         package_name_
     )
     print(download_message, end=" ")
     command = ["conda", "install", "-c", channel, package_name, "-y"]
-    log = subprocess.run(command, stdout=subprocess.PIPE, stderr=False)
+    log = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     print("done.")
     return log
 
 
 # check download --------------------------------------------------------------
-
 def _check_download_log(download_log):
 
     msg = download_log.stdout.decode("utf-8")
@@ -87,10 +85,16 @@ def _check_download_log(download_log):
 def _remove_package(package_name):
 
     command = ["conda", "remove", package_name, "-y"]
+    rm_log = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if rm_log.stdout.decode("utf-8").endswith("done\n"):
+        return rm_log, "removed"
+    else:
+        return rm_log, "there was a problem during removal..."
     try:
-        rm_log = subprocess.run(command, stdout=subprocess.PIPE, stderr=False)
+        rm_log = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         if rm_log.stdout.decode("utf-8").endswith("done\n"):
             return rm_log, "removed"
     except:
         package_name = licorice_font.font_format(package_name, ["BOLD", "GREEN"])
         print("Could not remove: {}. Maybe it is not installed.".format(package_name))
+        return None, "not_removed"
